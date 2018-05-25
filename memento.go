1cc6caa09a9f1581385998160e5e9718ea342ac4
Patrick Yeon <patrickyeon@gmail.com>
2018-05-24 23:45:11 -0700

    well we can sort of spider through a timegate

--- memento.go
+++ memento.go
@@ -1,8 +1,13 @@
 package rssrerun
 
 import (
+    "bufio"
+    "bytes"
     "errors"
+    "io"
+    "net/http"
     "regexp"
+    "strings"
 )
 
 var linkSplitter = regexp.MustCompile("<([^>]*)>((.|\\n)*)")
@@ -39,4 +44,123 @@ func ParseMemento(s string) (Memento, error) {
         }
     }
     return Memento{res[1], paramMap}, nil
+}
+
+type TimeMap struct {
+	Links []Memento
+}
+
+func ParseTimeMap(r io.Reader) (*TimeMap, error) {
+    retval := TimeMap{[]Memento{}}
+    scanner := bufio.NewScanner(r)
+    var agg bytes.Buffer
+    for scanner.Scan() {
+        _, _ = agg.WriteString(scanner.Text())
+        ss := agg.String()
+        matched, _ := regexp.MatchString(".*,\\s*$", ss)
+        if matched || scanner.Err() != nil {
+            mem, err := ParseMemento(agg.String())
+            if err != nil {
+                return nil, err
+            }
+            retval.Links = append(retval.Links, mem)
+            agg.Reset()
+        }
+    }
+    return &retval, nil
+}
+
+func FetchTimeMap(url string) (*TimeMap, error) {
+    res, err := http.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    if res.StatusCode != 200 {
+        return nil, errors.New("non-200 HTTP code")
+    }
+    return ParseTimeMap(res.Body)
+}
+
+func inArray(needle string, haystack []string) bool {
+    for _, checkme := range haystack {
+        if needle == checkme {
+            return true
+        }
+    }
+    return false
+}
+
+// Two options here, either the timemap references all the timemaps that went
+// into it, or it references none of them, as they are not places to get more
+// information that's not here.
+// I mean, I guess the result can be either of those, but it would inform the
+// naive approach.
+// I prefer that the result does not contain redundant references.
+// This means that part of every call needs to return the timemaps traversed, so
+// that we know not to call them later. So there's SpiderTimeMap, that calls
+// spiderRecurse (or something), which can return (mementos, tmaps, error).
+
+func SpiderTimeMap(url string) (*TimeMap, error) {
+    tmap, err := recurseSpider(url, nil)
+    if err != nil {
+        return nil, err
+    }
+    nDel := 0
+    for i := 0; i < len(tmap.Links) - nDel; {
+        if tmap.Links[i].Params["rel"] == "timemap" {
+            if i + 1 + nDel >= len(tmap.Links) {
+                tmap.Links[i] = nilMemento()
+            } else {
+                tmap.Links[i] = tmap.Links[i + 1 + nDel]
+            }
+            nDel++
+        } else {
+            i++
+        }
+    }
+    tmap.Links = tmap.Links[:len(tmap.Links) - nDel]
+    // TODO: sort the mementos
+    return tmap, nil
+}
+
+func recurseSpider(url string, skip_urls []string) (*TimeMap, error) {
+    skip_urls = append(skip_urls, url)
+    tm, err := FetchTimeMap(url)
+    if err != nil {
+        return nil, err
+    }
+    for _, link := range tm.GetTimeMaps() {
+        if !inArray(link.Url, skip_urls) {
+            subtm, err := recurseSpider(link.Url, skip_urls)
+            if err != nil {
+                return nil, err
+            }
+            tm.Links = append(tm.Links, subtm.GetTimeMaps()...)
+            tm.Links = append(tm.Links, subtm.GetMementos()...)
+            for _, tmap := range subtm.GetTimeMaps() {
+                skip_urls = append(skip_urls, tmap.Url)
+            }
+        }
+    }
+    return tm, nil
+}
+
+func (tm *TimeMap) GetTimeMaps() []Memento {
+    retval := []Memento{}
+    for _, link := range tm.Links {
+        if link.Params["rel"] == "timemap" {
+            retval = append(retval, link)
+        }
+    }
+    return retval
+}
+
+func (tm *TimeMap) GetMementos() []Memento {
+    retval := []Memento{}
+    for _, link := range tm.Links {
+        if strings.HasSuffix(link.Params["rel"], "memento") {
+            retval = append(retval, link)
+        }
+    }
+    return retval
 }
--- memento_test.go
+++ memento_test.go
@@ -1,7 +1,14 @@
 package rssrerun
 
 import (
+    "fmt"
+    "io"
+    "net/http"
+    "net/http/httptest"
+    "os"
+    "strings"
     "testing"
+    "time"
 )
 
 type testMemento struct {
@@ -42,3 +49,136 @@ func TestParseLinks (t *testing.T) {
     }
 }
 
+func TestExtractLinks (t *testing.T) {
+    reader, err := os.Open("testdat/timemap.txt")
+    if err != nil {
+        t.Fatal(err)
+    }
+    tm, err := ParseTimeMap(reader)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if nlinks := len(tm.Links); nlinks != 7 {
+        t.Fatalf("Didn't parse all links. Expected 7, got %d.", nlinks)
+    }
+}
+
+func TestGetMementos (t *testing.T) {
+    reader, err := os.Open("testdat/timemap.txt")
+    if err != nil {
+        t.Fatal(err)
+    }
+    tm, err := ParseTimeMap(reader)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if nmem := len(tm.GetMementos()); nmem != 4 {
+        t.Fatalf("Didn't get all mementos. Expected 4, got %d.", nmem)
+    }
+}
+
+func TestTMap(t *testing.T) {
+    tm := initTMap("http://example.com", "http://timegate.com/example.com")
+    tm.addMemento("http://timegate.com/2/example.com",
+                  time.Date(2018, 4, 4, 0, 0, 0, 0, time.UTC))
+    tm.addMemento("http://timegate.com/1/example.com",
+                  time.Date(2018, 2, 3, 0, 0, 0, 0, time.UTC))
+    res, err := ParseTimeMap(tm.toReader())
+    if err != nil {
+        t.Fatal(err)
+    }
+    if nlinks := len(res.Links); nlinks != 4 {
+        t.Errorf("Didn't get all links. Expected 4, got %d.", nlinks)
+    }
+    if nmem := len(res.GetMementos()); nmem != 2 {
+        t.Fatalf("Didn't get all mementos. Expected 2, got %d.", nmem)
+    }
+}
+
+func TestSeriesOfTimeMaps(t *testing.T) {
+    mementos := []string{
+        "http://timegate.com/1/example.com",
+        "http://timegate.com/2/example.com",
+        "http://timegate.com/3/example.com",
+        "http://timegate.com/4/example.com",
+        "http://timegate.com/5/example.com",
+    }
+
+    tm1 := initTMap("http://example.com", "http://timegate.com/example.com")
+    for i := 0; i < 2; i++ {
+        tm1.addMemento(mementos[i], time.Date(2018, 3, 30 - i,
+                                              0, 0, 0, 0, time.UTC))
+    }
+    ts1 := tmServer(tm1)
+    defer ts1.Close()
+    tm2 := initTMap("http://example.com", "http://timegate.com/b/example.com")
+    for i := 2; i < 5; i++ {
+        tm2.addMemento(mementos[i], time.Date(2018, 3, 30 - i,
+                                              0, 0, 0, 0, time.UTC))
+    }
+    tm2.addTMap(ts1.URL)
+    ts2 := tmServer(tm2)
+    defer ts2.Close()
+
+    timemap, err := SpiderTimeMap(ts2.URL)
+    if err != nil {
+        t.Fatal(err)
+    }
+    // should have 7 links, "original", "timegate", then 5 mementos
+    // the other "timegate" and the "timemap"s get swallowed silently
+    if nlinks := len(timemap.Links); nlinks != 7 {
+        t.Errorf("Didn't get all links. Expected 7, got %d.", nlinks)
+    }
+    if nmems := len(timemap.GetMementos()); nmems != 5 {
+        t.Errorf("Didn't get all mementos. Expected 5, got %d.", nmems)
+    }
+}
+
+// tools used to build timemaps, test the fetching, etc.
+type mmnto struct {
+    url string
+    dt time.Time
+}
+
+type tMap struct {
+    original string
+    timegate string
+    timemaps []string
+    mementos []mmnto
+}
+
+func initTMap(original string, timegate string) *tMap {
+    return &tMap{original, timegate, nil, nil}
+}
+func (t *tMap) addMemento(url string, dt time.Time) {
+    t.mementos = append(t.mementos, mmnto{url, dt})
+}
+func (t *tMap) addTMap(url string) {
+    t.timemaps = append(t.timemaps, url)
+}
+func (t *tMap) toReader() io.Reader {
+    return strings.NewReader(t.toString())
+}
+func (t *tMap) toString() string {
+    links := ""
+    for _, tmap := range(t.timemaps) {
+        links = strings.Join([]string{links,
+            "<", tmap, ">; rel=\"timemap\",\n"}, "")
+    }
+    for _, mm := range(t.mementos) {
+        links = strings.Join([]string{links,
+            "<", mm.url, ">; rel=\"memento\";",
+            "datetime=\"", mm.dt.String(), "\",\n"}, "")
+    }
+    return strings.Join(append([]string{
+        "<", t.original, ">; rel=\"original\",\n",
+        "<", t.timegate, ">; rel=\"timegate\",\n",
+        }, links), "")
+}
+
+func tmServer(t *tMap) *httptest.Server {
+    return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter,
+                                                    r *http.Request) {
+            fmt.Fprint(w, t.toString())
+    }))
+}
